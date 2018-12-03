5731e51e2fe55e9c34894fc833a04bfab0bfa66f
HarikaKuladevi <42564584+HarikaKuladevi@users.noreply.github.com>
2018-12-02 20:12:35 -0600

    updated

--- ssl-certificate.sh
+++ ssl-certificate.sh
@@ -13,6 +13,8 @@ sudo systemctl restart httpd.service
 sudo firewall-cmd --permanent --add-service=https
 sudo firewall-cmd --reload  
 
+sudo mkdir /etc/ssl/private
+
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj "/C=US/ST=Illinois/L=Chicago/O=IIT-Company/OU=Org/CN=www.user.com"
 
 
@@ -23,8 +25,8 @@ DocumentRoot /var/www/html
 SSLEngine on
 SSLProtocol all -SSLv2 -SSLv3
 SSLCipherSuite HIGH:MEDIUM:!aNULL:!MD5
-SSLCertificateFile /etc/httpd/ssl/apache-selfsigned.crt
-SSLCertificateKeyFile /etc/httpd/ssl/apache-selfsigned.key
+SSLCertificateFile /etc/ssl/certs/apache-selfsigned.crt
+SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key
 </VirtualHost>
 <Directory "/var/www/html">
 Require all granted
