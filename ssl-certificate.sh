d0bdd36913ec56a4bb28497fbabac4412cf1c6e8
HarikaKuladevi <42564584+HarikaKuladevi@users.noreply.github.com>
2018-12-02 19:53:30 -0600

    updated the code

--- ssl-certificate.sh
+++ ssl-certificate.sh
@@ -13,24 +13,8 @@ sudo systemctl restart httpd.service
 sudo firewall-cmd --permanent --add-service=https
 sudo firewall-cmd --reload  
 
-echo "
-#!/bin/bash
-sudo mkdir /etc/httpd/ssl
-sudo cd /etc/httpd/ssl
-echo -e "Enter your virtual host FQDN: \n"
-read cert
-sudo openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out $cert.key
-sudo chmod 600 $cert.key
-sudo openssl req -new -key $cert.key -out $cert.csr
-sudo openssl x509 -req -days 365 -in $cert.csr -signkey $cert.key -out $cert.crt
-echo -e " The Certificate and Key file for $cert has been generated!\n"
-ls -la /etc/httpd/ssl
-exit 0" | sudo /usr/local/bin/apache_ssl
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj "/C=US/ST=Illinois/L=Chicago/O=IIT-Company/OU=Org/CN=www.user.com"
 
-
-sudo chmod +x /usr/local/bin/apache_ssl
-
-apache_ssl
 
 echo " 
 VirtualHost *:443>
@@ -39,10 +23,9 @@ DocumentRoot /var/www/html
 SSLEngine on
 SSLProtocol all -SSLv2 -SSLv3
 SSLCipherSuite HIGH:MEDIUM:!aNULL:!MD5
-SSLCertificateFile /etc/httpd/ssl/apachecert.crt
-SSLCertificateKeyFile /etc/httpd/ssl/apachecert.key
+SSLCertificateFile /etc/httpd/ssl/apache-selfsigned.crt
+SSLCertificateKeyFile /etc/httpd/ssl/apache-selfsigned.key
 </VirtualHost>
-
 <Directory "/var/www/html">
 Require all granted
 </Directory>" | sudo /etc/httpd/conf.d/sitel.conf
