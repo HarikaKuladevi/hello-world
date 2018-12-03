497fd109c4015fd886ad4a4bae630384294d84f7
HarikaKuladevi <42564584+HarikaKuladevi@users.noreply.github.com>
2018-12-03 08:41:02 -0600

    updated the script

--- ssl-certificate.sh
+++ ssl-certificate.sh
@@ -17,20 +17,16 @@ sudo mkdir /etc/ssl/private
 
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj "/C=US/ST=Illinois/L=Chicago/O=IIT-Company/OU=Org/CN=www.user.com"
 
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
-echo " 
-VirtualHost *:443>
-ServerName localhost
-DocumentRoot /var/www/html
-SSLEngine on
-SSLProtocol all -SSLv2 -SSLv3
-SSLCipherSuite HIGH:MEDIUM:!aNULL:!MD5
-SSLCertificateFile /etc/ssl/certs/apache-selfsigned.crt
-SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key
-</VirtualHost>
-<Directory "/var/www/html">
-Require all granted
-</Directory>" | sudo /etc/httpd/conf.d/sitel.conf
+cat /etc/ssl/certs/dhparam.pem | sudo tee -a /etc/ssl/certs/apache-selfsigned.crt
+
+sudo cp /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.conf.bak
+sudo sed -i -e 's/#ServerName www.example.com:443/ServerName localhost/g' /etc/httpd/conf.d/ssl.conf
+sudo sed -i -e 's/SSLCertificateKeyFile /etc/pki/tls/private/localhost.key/SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key/g' /etc/httpd/conf.d/ssl.conf
+sudo sed -i -e 's/SSLCertificateFile /etc/pki/tls/certs/localhost.crt/SSLCertificateFile /etc/ssl/certs/apache-selfsigned.crt/g' /etc/httpd/conf.d/ssl.conf
+
+sudo apachectl configtest
 
 sudo systemctl restart httpd.service
 
